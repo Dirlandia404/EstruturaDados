6c707295ff8a12cdce2e7817291871c59f715b1f
Dirlandia <60372480+Dirlandia404@users.noreply.github.com>
2021-06-15 19:46:06 -0300

    Delete Aulas-Extra/.history directory

--- Aulas-Extra/.history/operadores_20210601225458.cpp
+++
@@ -1,5 +0,0 @@
-#include <iostream> //biblioteca padrão de entrada e saida de c++
-#include <cstdlib> //biblioteca de conversão
-#include <locale> //biblioteca de acentuação
-#include <string> //biblioteca de string
-
